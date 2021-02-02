a51b2b7ee19310952a8536d742b2c533885a035b
rimever <mot_siva_ordin@hotmail.com>
2021-02-02 21:48:21 +0900

    少しソースコードをアレンジ

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int, char ** const)
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Hey",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,0);
+    SDL_Window* window = SDL_CreateWindow("Hello SDL",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,0);
     SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
 
 
@@ -18,7 +18,7 @@ int main(int, char ** const)
             if (ev.type == SDL_QUIT)
                 return 0;
         }
-        SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
+        SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
         SDL_RenderDrawLine(render,10, 10, 400, 400);
         SDL_RenderPresent(render);
     }
